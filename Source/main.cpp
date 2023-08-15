ef7e09c97255d004627e9224bcd273db131fec0a
samenachan <x4samezame@gmail.com>
2023-08-15 23:58:25 +0900

    Add GLFW

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,10 +2,26 @@
 #include <vector>
 #include <string>
 #include <windows.h>
+#include "GLFW/glfw3.h"
 
 int main()
 {
 	std::cout << "Begin program" << std::endl;
+
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Arabesque", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
 	std::cout << "End program" << std::endl;
+
 	return 0;
 }
